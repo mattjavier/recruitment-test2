8741197c2c5a158dec81895f73a908e0a995fe98
Matt Javier <matthewrjavier@gmail.com>
2021-01-29 22:18:35 -0800

    frontend test

--- full-stack-recruitment-test/pages/_app.js
+++ full-stack-recruitment-test/pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import theme from '../src/theme.js'
+
+import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
+
--- full-stack-recruitment-test/pages/index.js
+++ full-stack-recruitment-test/pages/index.js
@@ -1,54 +1,57 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 
-export default function Home() {
+import Header from '../src/components/Header'
+import Flights from '../src/components/Flights'
+
+import flights from '../public/flights.json'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default
+  },
+  main: {
+    flexGrow: 1,
+    padding: theme.spacing(3, 0),
+    width: '100%'
+  }
+}))
+
+const Home = props => {
+
+  const classes = useStyles()
+
   return (
-    <div className={styles.container}>
+    <Grid 
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={classes.container}
+    >
       <Head>
-        <title>Create Next App</title>
+        <title>Skyscanner</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      {/* Header */}
+      <Header />
+      <Grid
+        container
+        direction="column"
+        justify="center" 
+        alignItems="center"
+        className={classes.main}
+      >
+        {/* List of Itineraries */}
+        <Flights 
+          itineraries={props.itineraries}
+          legs={props.legs}
+        />
+      </Grid>
 
       <footer className={styles.footer}>
         <a
@@ -60,6 +63,19 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
-    </div>
+    </Grid>
   )
 }
+
+export const getStaticProps = async () => {
+  const json = flights
+
+  return {
+    props: { 
+      itineraries: json.itineraries, 
+      legs: json.legs
+    }
+  }
+}
+
+export default Home
