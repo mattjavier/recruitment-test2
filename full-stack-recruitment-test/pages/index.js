e956ba66b7173bfdd7a6a64b0f65c5696aad8e47
Matt Javier <matthewrjavier@gmail.com>
2021-01-31 17:48:37 -0800

    filtering agents and dsiplaying prices

--- full-stack-recruitment-test/pages/index.js
+++ full-stack-recruitment-test/pages/index.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Home = props => {
-  console.log(props)  
+  
   const classes = useStyles()
 
   return (
--- full-stack-recruitment-test/src/components/Flights.js
+++ full-stack-recruitment-test/src/components/Flights.js
@@ -3,14 +3,37 @@ import { useRouter } from 'next/router'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import FormControl from '@material-ui/core/FormControl'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import Itinerary from './Itinerary'
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    padding: theme.spacing(3),
     backgroundColor: theme.palette.background.default,
+  },
+  formControl: {
+    minWidth: 120,
+    width: '90%',
+    margin: theme.spacing(1),
+    padding: 0,
+  },
+  formText: {
+    color: theme.palette.primary.main
+  },
+  display: {
+    padding: theme.spacing(3),
     width: '100%'
+  },
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
   }
 }))
 
@@ -20,42 +43,102 @@ const Flights = props => {
 
   const router = useRouter()
 
+  let agentChoices = props.itineraries.map(itinerary => itinerary.agent)
+  
+  const [agentState, setAgents] = useState([])
+  const [selectedState, setSelected] = useState({
+    selected: []
+  })
   const [itineraryState, setItineraryState] = useState({
     totalPrice: '',
     avgPrice: ''
   })
+  
+  const getSelection = () => {
+    let selected = props.itineraries.filter(itinerary => itinerary.agent === agentState)
+    console.log(selected)
+    setSelected({...selectedState, selected })
+  }
 
   useEffect(() => {
+    getSelection()
+
     let totalPrice = 0
-    for (let i = 0; i < props.itineraries.length; i++) {
-      totalPrice += props.itineraries[i].price
+    for (let i = 0; i < selectedState.selected.length; i++) {
+      totalPrice += selectedState.selected[i].price
     }
 
-    let avgPrice = totalPrice / props.itineraries.length
+    let avgPrice = totalPrice / selectedState.selected.length
 
     setItineraryState({ ...itineraryState, totalPrice, avgPrice })
     router.push('/')
   }, [])
+  
+  const handleAgentChange = (event) => {
+    setAgents(event.target.value)
+  }
+
 
   return (
     <Grid
       container
       direction="column"
-      justify="center"
-      alignItems="center" 
+      justify="center" 
+      alignItems="center"
       className={classes.container}
     >
-      {
-        props.itineraries.map(itinerary => {
-          return (
-            <Itinerary 
-              key={itinerary._id}
-              itinerary={itinerary}
-              legs={props.legs}
-            />
-          )
-        })
-      }
+      <FormControl className={classes.formControl}>
+        <InputLabel id="agent-label" className={classes.formText}>Agent</InputLabel>
+        <Select
+          name="agents"
+          id="select"
+          labelId="agent-label"
+          value={agentState}
+          onChange={handleAgentChange}
+          input={<Input />}
+        >
+          <MenuItem
+            key="default"
+            value="default"
+            className={classes.formText}
+          >
+            Agent
+          </MenuItem>
+          {
+            agentChoices.map(agent => {
+              return (
+                <MenuItem
+                  key={agent}
+                  value={agent}
+                  className={classes.formText}
+                >
+                  {agent}
+                </MenuItem>
+              )
+            })
+          }
+        </Select>
+        <FormHelperText className={classes.formText}>Filter by agent</FormHelperText>
+      </FormControl>
+      <Grid
+        className={classes.display}
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        {
+          selectedState.selected.map(itinerary => {
+            return (
+              <Itinerary 
+                key={itinerary._id}
+                itinerary={itinerary}
+                legs={props.legs}
+              />
+            )
+          })
+        }
+      </Grid>
       <Grid
         container
         direction="column"
